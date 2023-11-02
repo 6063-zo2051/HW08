5a7c198223999e007327690651b6775544dde24c
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-11-01 20:59:23 -0400

    HW08 Iteration 1

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,41 @@
+let img;
+
+function preload() {
+  img = loadImage('PietImage.jpg');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(img.width, img.height);
+  image(img, 0, 0);
+
+replaceColors(color(255, 0, 0), color(0, 255, 0));
+replaceColors(color(255, 255, 0), color(0, 0, 255));
+replaceColors(color(0, 0, 255), color(255, 255, 0));
+
+image(img, 0, 0);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+}
+
+function replaceColors (targetColor, replacementColor) {
+  img.loadPixels();
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    let r = pixels[i];
+    let g = pixels[i + 1];
+    let b = pixels[i + 2];
+
+    if (colorMatches(targetColor, color(r, g, b))) {
+      img.pixels[i] = red(replacementColor);
+      img.pixels[i+1] = green(replacementColor);
+      img.pixels[i+2] = blue(replacementColor);
+    }
+  }
+
+  img.updatePixels();
+}
+
+function colorMatches (color1, color2) {
+return red(color1) === red(color2) && green(color1) === green(color2) && blue(color1) === blue(color2);
 }
