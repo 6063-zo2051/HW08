34307cee2411b9aa22a6a77795d36e26a351797e
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-11-03 13:22:59 -0400

    HW 08 Iteration Part 2

--- HW08Part2/sketch.js
+++ HW08Part2/sketch.js
@@ -1,11 +1,49 @@
+let song;
+let peaks;
+let lineX = 0;
+let buttonClick;
+let isPlaying;
+
+function preload() {
+  song = loadSound("./hotel.mp3", loaded);
+}
+
+function loaded() {
+  if (song.isLoaded()) {
+    peaks = song.getPeaks();
+    song.play();
+  }
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+
+buttonClick = createButton("Create Round");
+buttonClick.mousePressed(playButton);
+buttonClick.position(width / 4, height / 2.25);
+buttonClick.style('font-size', '30px');
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+  background(220, 20, 120);
+
+  if ( peaks ) {
+    let t = map(song.currentTime(), 0, song.duration(), 0, width);
+    lineX = lerp(lineX, t, 0.1);
+
+    stroke(0);
+    strokeWeight(5);
+    line(lineX, 0, lineX, height);
+  }
 }
+
+function playButton () {
+  if (isPlaying) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+
+
--- README.md
+++ README.md
@@ -8,5 +8,7 @@ I decided to change red to blue, green to yellow, and blue to a random color ini
 
 When I ran my sketch on a different image, the colors did change as expected but the image did not resize. I am not sure why this is.
 
+## Part 2
 
+I picked an interlude by one of my favorite artists Mariah the Scientist. I just made the visualization a moving line as I had struggled with getting it to work due to the incorrect html line in my index section. I made another button click that created a round of the song. The line moves back as you play a new round of song. I thought that this created a cool mixing effect and could probably go further by creating a DJ mixing table, maybe for a furuture project of the final!
 
