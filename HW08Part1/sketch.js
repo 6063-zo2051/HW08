c438642c1e6be026d57fb4a40dfceb7f2c824aa2
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-11-03 12:12:06 -0400

    HW 08 Iteration 4

--- HW08Part1/sketch.js
+++ HW08Part1/sketch.js
@@ -1,6 +1,7 @@
 let oImage;
 let mImage;
-//let buttonClick;
+let buttonClick;
+let randomColor;
 
 function preload() {
   oImage = loadImage("./PietImage.jpg");
@@ -23,15 +24,27 @@ function setup() {
 
 mImage = oImage.get();
 
-//buttonClick = createButton("Change Color");
-//buttonClick.mousePressed(changeBlue);
+newColor();
 
+buttonClick = createButton("Change Color");
+buttonClick.mousePressed(newColor);
+buttonClick.position(width / 2, height / 2);
+buttonClick.style('font-size', '30px');
+buttonClick.style('background-color', randomColor);
+
+}
+
+function newColor() {
+  randomColor = color(random(255), random(255), random(255));
 }
 
 function draw() {
   background(255);
 
   mImage.loadPixels();
+  let randomRed = red(randomColor);
+  let randomGreen = green(randomColor);
+  let randomBlue = blue(randomColor);
 
   for (let i = 0; i < mImage.pixels.length; i += 4) {
     rValue = oImage.pixels[i + 0];
@@ -40,14 +53,18 @@ function draw() {
 
     maxColorValue = max(rValue, bValue, gValue);
 
-    if (maxColorValue == rValue && rValue > gValue * 4 ) { //if red, change to blue
+    if (maxColorValue == rValue && rValue > gValue * 3 && rValue > bValue * 3 ) { //if red, change to blue
       mImage.pixels[i + 0] = 19;
       mImage.pixels[i + 1] = 87;
       mImage.pixels[i + 2] = 238;
-    } else if (maxColorValue == gValue) { //if green, change to yellow
+    } else if (maxColorValue == gValue && gValue > bValue * 2 && gValue > rValue * 2) { //if green, change to yellow
       mImage.pixels[i + 0] = 238;
       mImage.pixels[i + 1] = 227;
       mImage.pixels[i + 2] = 19;
+    } else if (maxColorValue == bValue && bValue > rValue && bValue > gValue) {
+      mImage.pixels[i + 0] = randomRed;
+      mImage.pixels[i + 1] = randomGreen;
+      mImage.pixels[i + 2] = randomBlue;
     }
   }
 
@@ -55,10 +72,5 @@ function draw() {
 
   image(mImage, 0, 0);
 
-  // buttonClick.position(width / 2, height / 2);
 }
-
-//function changeBlue() {
- // mImage.pixels[i + 1] = random(255);
-//}
 
